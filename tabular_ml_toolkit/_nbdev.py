516b3c90ec92bf431cf7bd96ae24494babb33391
pankajmathur <pankymathur@gmail.com>
2021-10-23 13:13:35 -0700

    updated name of notebooks to camel format

--- tabular_ml_toolkit/_nbdev.py
+++ tabular_ml_toolkit/_nbdev.py
@@ -2,12 +2,15 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"DataFrameLoader": "00_dataframeloader.ipynb",
-         "PreProcessor": "01_preprocessor.ipynb",
-         "MLPipeline": "03_MLPipeline.ipynb"}
+index = {"DataFrameLoader": "00_DataFrameLoader.ipynb",
+         "PreProcessor": "01_PreProcessor.ipynb",
+         "MLPipeline": "03_MLPipeline.ipynb",
+         "model": "03_MLPipeline.ipynb",
+         "pl": "03_MLPipeline.ipynb",
+         "preds": "03_MLPipeline.ipynb"}
 
-modules = ["dataframeloader.py",
-           "preprocessor.py",
+modules = ["DataFrameLoader.py",
+           "PreProcessor.py",
            "MLPipeline.py"]
 
 doc_url = "https://psmathur.github.io/tabular_ml_toolkit/"

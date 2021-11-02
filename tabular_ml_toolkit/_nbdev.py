42cb2e86583dc92e0b58b9963bf3fec158b3308c
pankajmathur <pankymathur@gmail.com>
2021-11-02 10:24:57 -0700

    submitted 0.07 Pypi & updated TPS Tutorial

--- tabular_ml_toolkit/_nbdev.py
+++ tabular_ml_toolkit/_nbdev.py
@@ -5,6 +5,10 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"DataFrameLoader": "00_DataFrameLoader.ipynb",
          "PreProcessor": "01_PreProcessor.ipynb",
          "MLPipeline": "02_MLPipeline.ipynb",
+         "DIRECTORY_PATH": "03_Tutorial.ipynb",
+         "TRAIN_FILE": "03_Tutorial.ipynb",
+         "TEST_FILE": "03_Tutorial.ipynb",
+         "SAMPLE_SUB_FILE": "03_Tutorial.ipynb",
          "scikit_model": "03_Tutorial.ipynb",
          "sci_ml_pl": "03_Tutorial.ipynb",
          "xgb_model": "03_Tutorial.ipynb",
@@ -13,7 +17,8 @@ index = {"DataFrameLoader": "00_DataFrameLoader.ipynb",
 modules = ["DataFrameLoader.py",
            "PreProcessor.py",
            "MLPipeline.py",
-           "Tutorial.py"]
+           "Tutorial.py",
+           "Kaggle_TPS_Tutorial.py"]
 
 doc_url = "https://psmathur.github.io/tabular_ml_toolkit/"
 
